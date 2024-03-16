9274cefa5a8f2a245c289a90d8979c1a74fc449a
Bipesh <bipesh@gmail.com>
2024-03-16 13:47:34 +0545

    Our First Commit

---
+++ src/app/components/hero/Hero.js
@@ -0,0 +1,76 @@
+"use client";
+import React, { useState } from "react";
+import { Chakra_Petch } from "next/font/google";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+
+const chakra_petch = Chakra_Petch({ subsets: ["latin"], weight: ["600"] });
+
+export default function Hero() {
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true,
+    },
+    [
+      (slider) => {
+        let timeout;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 2000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      },
+    ]
+  );
+
+  return (
+    <div>
+      <div className=" w-full h-9v relative">
+        <div className="px-5 flex items-center justify-between">
+          <div className="text-black flex flex-col gap-y-10 px-5">
+            <h2
+              className={`text-4xl uppercase leading-snug ${chakra_petch.className}`}
+            >
+              {" "}
+              <span className=" text-blue-500">welcome</span> to <br /> the
+              parking <br /> management system
+            </h2>
+            <p className=" leading-snug text-lg">
+              Seamlessly streamline your parking operations with our intuitive
+              web app. Simplify reservations, optimize space utilization, and
+              enhance customer satisfaction.{" "}
+            </p>
+          </div>
+          <div ref={sliderRef} className="keen-slider">
+            <div className="keen-slider__slide number-slide1"><img src="./images/hero-img.png" alt="" /></div>
+            <div className="keen-slider__slide number-slide2"><img src="./images/hero-img-1.png" alt="" /></div>
+            <div className="keen-slider__slide number-slide3"><img src="./images/hero-img-2.png" alt="" /></div>
+           
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/navigation/Navigation.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+import Button from "../button/Button";
+
+
+export default function Navigation() {
+  
+  return (
+    <div>
+      <div className="navigation-bar py-2 bg-gray-950 text-white ">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="logo">
+               <Link href={'/'}> <img src="./images/logo-img.png" alt="" /></Link> 
+          </div>
+        <div className="nav-items flex gap-8 text-sm uppercase ">
+            <Link href={""} className="hover:scale-105 transition-all">Your Dashboard</Link>
+            <Link href={""} className="hover:scale-105 transition-all">About Us</Link>
+            <Link href={""} className="hover:scale-105 transition-all">Contact</Link>
+          </div>
+          <div className="login-register flex gap-6 text-sm">
+            <Link href={"./login"}><Button title={"Login"} /></Link>
+            <Link href={""}><Button title={"Register"}/></Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

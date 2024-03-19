150fd0cb907bd3861a3830259042c18416766611
Bipesh <bipesh@gmail.com>
2024-03-19 21:15:52 +0545

    fourth commit

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -1,19 +1,25 @@
 import React from "react";
 import Link from "next/link";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className=" bg-gray-950">
-      <div className="container mx-auto px-10 py-10 flex gap-10 flex-wrap">
+      <div className="container border-b border-b-gray-700 mx-auto px-10 py-10 flex gap-10 flex-wrap">
         <div className="footer-logo w-2/5 text-sm flex flex-col gap-y-5">
           <img src="./images/logo-img.png" width={100} alt="" />
           <p className="text-white">
-            <span className=" text-blue-400 leading-loose">Unlock Convenience</span>: Seamlessly Reserve, Effortlessly Pay, and Stay
-            Informed with Our Smart Parking Solutions.
+            <span className=" text-blue-400 leading-loose font-semibold">
+              Unlock Convenience
+            </span>
+            : Seamlessly Reserve, Effortlessly Pay, and Stay Informed with Our
+            Smart Parking Solutions.
           </p>
         </div>
         <div className="footer-navigation text-blue-400 flex flex-col gap-y-5 text-sm">
@@ -43,12 +49,24 @@ export default function Footer() {
         <div className="social-media text-sm text-blue-400 flex flex-col gap-y-5">
           <h2 className="text-white font-semibold">Follow Us</h2>
           <div className="social-media-icons flex text-lg gap-x-5">
-            <FaFacebook />
-            <FaInstagram />
-            <FaLinkedin />
-            <FaWhatsapp />
+            <Link href={""} className="hover:scale-105 transition-all">
+              <FaFacebook />
+            </Link>
+            <Link href={""} className="hover:scale-105 transition-all">
+              <FaInstagram />
+            </Link>
+            <Link href={""} className="hover:scale-105 transition-all">
+              <FaLinkedin />
+            </Link>
+            <Link href={""} className="hover:scale-105 transition-all">
+              <FaWhatsapp />
+            </Link>
           </div>
         </div>
+      </div>
+      <div className="copyright flex pt-10 pb-5 justify-center items-center gap-x-1 text-white">
+        <FaRegCopyright className=" text-blue-400" />
+        <p className="uppercase text-sm">All rights reserved.</p>
       </div>
     </div>
   );
